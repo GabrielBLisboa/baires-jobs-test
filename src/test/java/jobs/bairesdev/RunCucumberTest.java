42a31be9fac0c1f8a33c28fe07b21364c2102be7
Gabriel B. Lisboa <113642602+GabrielBLisboa@users.noreply.github.com>
2023-06-12 12:16:04 -0300

    Update RunCucumberTest.java

--- src/test/java/jobs/bairesdev/RunCucumberTest.java
+++ src/test/java/jobs/bairesdev/RunCucumberTest.java
@@ -6,7 +6,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("jobs/bairesdev")
+@SelectClasspathResource("features/search.feature")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-reports/cucumber.html")
 //@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber-reports/cucumber.json")
